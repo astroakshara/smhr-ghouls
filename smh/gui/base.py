Preliminary fixes: masks and normalization not overwritten when RV is changed; added 'synth' and 'ew' options for the abundance table; custom sigma for plotting

smh/gui/base.py
@@ -125,7 +125,6 @@ class SMHSpecDisplay(mpl.MPLWidget):
         self.session = session
         self.label_ymin = label_ymin
         self.label_ymax = label_ymax
-        
         # E. Holmbeck changed colors:
         self.acceptable_color = "#d92653"
         self.unacceptable_color = "#37ae91"
@@ -754,6 +753,7 @@ class SMHScatterplot(mpl.MPLWidget):
                  error_styles=None,
                  linefit_styles=None,
                  linemean_styles=None,
+                 sigma=2.5,
                  **kwargs):
         assert xattr in self.allattrs, xattr
         assert yattr in self.allattrs, yattr
@@ -770,6 +770,7 @@ class SMHScatterplot(mpl.MPLWidget):
         '''
         self.exattr = None
         self.eyattr = None
+        self.sigma = sigma
         
         super(SMHScatterplot, self).__init__(parent=parent,
                                              **kwargs)
@@ -838,7 +839,7 @@ class SMHScatterplot(mpl.MPLWidget):
                     self.ax.axhline(np.nan, **linemean_kw))
                 # E. Holmbeck added shading
                 fillmean_objs.append(
-                    self.ax.fill_between([0,0],[0,0],[0,0], color=linemean_kw["color"], alpha=0.15, lw=0))
+                    self.ax.fill_between([0,0],[0,0],[0,0], color=linemean_kw["color"], alpha=0.10, lw=0))
         
         ## Save graphic objects
         self._selected_points = self.ax.scatter([], [],
@@ -1008,13 +1009,17 @@ class SMHScatterplot(mpl.MPLWidget):
                 # E. Holmbeck added; shamelessly stolen: https://stackoverflow.com/questions/16120801/matplotlib-animate-fill-between-shape
                 if (fillmean is not None):
                     path = fillmean.get_paths()[0]
-                    y0new = [medy - 2.5*stdy]*2
-                    y1new = [medy + 2.5*stdy]*2
+                    y0new = [medy - self.sigma*stdy]*2
+                    y1new = [medy + self.sigma*stdy]*2
+                    #import pdb
+                    #pdb.set_trace()
+                    #self.edit_sigma.text()
                     xnew = list(self.ax.get_xlim())
                     v_x = np.hstack([xnew[0],xnew,xnew[-1],xnew[::-1],xnew[0]])
                     v_y = np.hstack([y1new[0],y0new,y0new[-1],y1new[::-1],y1new[0]])
                     vertices = np.vstack([v_x,v_y]).T
                     path.vertices = vertices
+                    self.ax.set_ylim(*[1.2*lim for lim in self.ax.get_ylim()])
                 
         style_utils.relim_axes(self.ax)
         self.reset_zoom_limits()

smh/gui/chemical_abundances.py
@@ -43,7 +43,7 @@ if sys.platform == "darwin":
         QtGui2.QFont.insertSubstitution(*substitute)
 
 _QFONT = QtGui2.QFont("Helvetica Neue", 10)
-_ROWHEIGHT = 20
+_ROWHEIGHT = 30
 DOUBLE_CLICK_INTERVAL = 0.1 # MAGIC HACK
 PICKER_TOLERANCE = 10 # MAGIC HACK
 
@@ -741,6 +741,8 @@ class ChemicalAbundancesTab(QtGui.QWidget):
         box = self.filter_combo_box
         box.clear()
         box.addItem("All")
+        box.addItem("Synthesized Lines")
+        box.addItem("EW Lines")
 
         all_species = set([])
         for spectral_model in self.full_measurement_model.spectral_models:
@@ -756,6 +758,7 @@ class ChemicalAbundancesTab(QtGui.QWidget):
             assert species == utils.element_to_species(elem)
             box.addItem(elem)
 
+    # E. Holmbeck added filters for Synth and EW
     def filter_combo_box_changed(self):
         elem = self.filter_combo_box.currentText()
         # Update the filter
@@ -768,8 +771,20 @@ class ChemicalAbundancesTab(QtGui.QWidget):
                 logger.debug(e)
                 logger.debug(self.measurement_model.filter_functions)
                 raise            
-        if elem in [None, "", "All"]:
+        if elem in [None, "", "All", "None"]:
             self.element_summary_text.setText("")
+        elif elem == "Synthesized Lines":
+            self.element_summary_text.setText("")
+            def filter_function(model):
+                if isinstance(model, SpectralSynthesisModel):
+                    return model.species
+            self.measurement_model.add_filter_function(elem, filter_function)
+        elif elem == "EW Lines":
+            self.element_summary_text.setText("")
+            def filter_function(model):
+                if isinstance(model, ProfileFittingModel):
+                    return model.species
+            self.measurement_model.add_filter_function(elem, filter_function)
         else:
             species = utils.element_to_species(elem)
             def filter_function(model):
@@ -778,7 +793,8 @@ class ChemicalAbundancesTab(QtGui.QWidget):
                 elif isinstance(model, SpectralSynthesisModel):
                     return np.any([species in specie for specie in model.species])
             self.measurement_model.add_filter_function(elem, filter_function)
-        self._currently_plotted_element = elem
+        if elem not in ["Synthezized Lines", "EW Lines"]:
+            self._currently_plotted_element = elem
         self.measurement_model.endResetModel()
         self.summarize_current_table()
         self.refresh_plots()
@@ -794,12 +810,26 @@ class ChemicalAbundancesTab(QtGui.QWidget):
             return np.nan
         return self.FeH
 
+    # E. Holmbeck added the synth and ew lines; TODO: make less hacky
     def summarize_current_table(self):
         elem = self.filter_combo_box.currentText()
         if elem is None or elem == "" or elem == "All":
             N = self.measurement_model.rowCount()
             self.element_summary_text.setText("N={} lines".format(N))
             return None
+        elif elem == "Synthesized Lines":
+            filtered = []
+            for line in self.parent.session.spectral_models:
+                if line.measurement_type == "syn": filtered.append(line)
+            self.element_summary_text.setText("N={} lines".format(len(filtered)))
+            return None
+        elif elem == "EW Lines":
+            filtered = []
+            for line in self.parent.session.spectral_models:
+                if line.measurement_type == "eqw": filtered.append(line)
+            self.element_summary_text.setText("N={} lines".format(len(filtered)))
+            return None
+        
         summary_dict = self.parent.session.summarize_spectral_models(organize_by_element=False)
         species = utils.element_to_species(elem)
         if species not in summary_dict:

smh/gui/rv.py
@@ -772,6 +772,8 @@ class RVTab(QtGui.QWidget):
         Correct the radial velocity of the observed spectra.
         """
 
+        rv_diff = np.float(self.rv_applied.text()) + np.float(self.parent.session.metadata["rv"]["rv_applied"])
+        
         self.parent.session.rv_correct(self.rv_applied.text())
 
         # Redshift the normalized order.
@@ -779,7 +781,8 @@ class RVTab(QtGui.QWidget):
 
         # Enable and update the normalization tab.
         self.parent.tabs.setTabEnabled(self.parent.tabs.indexOf(self) + 1, True)
-        self.parent.normalization_tab.update_rv_applied()
+        # New function E. Holmbeck added to keep masks
+        self.parent.normalization_tab.update_rv_applied_keep_masks(rv_diff)
 
         # Enable relevant menu actions.
         self.parent._action_fit_balmer_lines.setEnabled(True)