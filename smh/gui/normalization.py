RV bug 3/??; I think there was a wrong sign in v/c

smh/gui/normalization.py
@@ -971,8 +971,8 @@ class NormalizationTab(QtGui.QWidget):
         # Update the current order fit, and the view.
         self.update_order_index()
         # May 7 -- added these two lines back in.
-        #self.update_continuum_mask(refresh=False)
-        #self.fit_continuum(clobber=True)
+        self.update_continuum_mask(refresh=False)
+        self.fit_continuum(clobber=True)
         self.draw_order(refresh=False)
         self.draw_continuum(refresh=True)
 
@@ -1022,12 +1022,6 @@ class NormalizationTab(QtGui.QWidget):
         # mask
         
         # Get the applied velocity to shift some masks.
-        '''
-        try:
-            rv_applied = self.parent.session.metadata["rv"]["rv_applied"]
-        except (AttributeError, KeyError):
-            rv_applied = 0
-        '''
         rv_applied = self.parent.session.metadata["rv"].get("rv_applied", 0.0)
         # -----------------------------------------------------------------
         # E. Holmbeck added read-in BCV from header
@@ -1041,8 +1035,8 @@ class NormalizationTab(QtGui.QWidget):
         _ =self.parent.session.metadata["normalization"]["normalization_kwargs"]
         masked_regions = [
             #np.array(mask.get("rest_wavelength", [])),
-            np.array(mask.get("rest_wavelength", [])) * (1.0 - dop_shift/c),
-            np.array(mask.get("obs_wavelength", [])) * (1.0 - rv_applied/c),
+            np.array(mask.get("rest_wavelength", [])) * (1.0 + dop_shift/c),
+            np.array(mask.get("obs_wavelength", [])) * (1.0 + rv_applied/c),
             np.array(_[self.current_order_index].get("exclude", []))
         ]
         if "pixel" in mask:
@@ -1297,8 +1291,9 @@ class NormalizationTab(QtGui.QWidget):
             v = session.metadata["rv"]["rv_applied"]
         except (AttributeError, KeyError):
             v = 0
-
-        self.current_order._dispersion *= (1 - v/c)
+        
+        # TODO: Holmbeck note: this was -v/c...which is correct?
+        self.current_order._dispersion *= (1 + v/c)
 
         # Update the view if the input settings don't match the settings used
         # to normalize the current order.

smh/gui/rv.py
@@ -771,21 +771,17 @@ class RVTab(QtGui.QWidget):
         """
         Correct the radial velocity of the observed spectra.
         """
-    def correct_radial_velocity(self):
-        """
-        Correct the radial velocity of the observed spectra.
-        """
-        rv_diff = np.float(self.rv_applied.text()) + self.parent.session.metadata["rv"].get("rv_applied",0.0)
+        if "rv_applied" in  self.parent.session.metadata["rv"]:
+            rv_diff = np.float(self.rv_applied.text()) + self.parent.session.metadata["rv"]["rv_applied"]
+        else:
+            rv_diff = 0.0
         
+        # rv_applied is set in this function
         self.parent.session.rv_correct(self.rv_applied.text())
+        
         # Redshift the normalized order; will look for rv_applied.
         self.redraw_normalized_order(True)
         
-        # Holmbeck added these lines to resolve an RV bug.
-        #rv_diff = np.float(self.rv_applied.text()) + self.parent.session.metadata["rv"]["rv_applied"]
-        #print(self.parent.session.metadata["rv"]["rv_applied"], rv_diff)
-        self.parent.session.metadata["rv"]["rv_applied"] += np.float(self.rv_applied.text())
-
         # Enable and update the normalization tab.
         self.parent.tabs.setTabEnabled(self.parent.tabs.indexOf(self) + 1, True)
 